6794bbffe54dfc2a2b3df037bc3e857974643081
Victor Chinyavada <vchinyavada@zss.co.zw>
2022-03-09 10:46:39 +0200

    add heroku url

--- app/src/main/java/com/connectus/mobile/common/Constants.java
+++ app/src/main/java/com/connectus/mobile/common/Constants.java
@@ -1,7 +1,7 @@
 package com.connectus.mobile.common;
 
 public class Constants {
-    public static final String CORE_BASE_URL = "http://192.168.100.2:8080";
+    public static final String CORE_BASE_URL = "http://connect-us-service.herokuapp.com";
     public static final String GENERAL_TOPIC = "general";
     public static final String AGENT_TOPIC = "paymate";
 }
