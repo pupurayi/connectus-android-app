933ab97ae673de166bf4165f0bd95e59a4f3690b
VIctor Chinyavada <chinyavadav@gmail.com>
2022-05-03 06:48:20 +0200

    add avatar render

--- app/src/main/java/com/connectus/mobile/ui/user/UserDetailsFragment.java
+++ app/src/main/java/com/connectus/mobile/ui/user/UserDetailsFragment.java
@@ -3,6 +3,8 @@ package com.connectus.mobile.ui.user;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +30,9 @@ import com.connectus.mobile.utils.Utils;
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Date;
 
 /**
@@ -45,6 +51,7 @@ public class UserDetailsFragment extends Fragment {
     SharedPreferencesManager sharedPreferencesManager;
 
     private UserViewModel userViewModel;
+    private UserDto userDto;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,7 +71,7 @@ public class UserDetailsFragment extends Fragment {
         pd = new ProgressDialog(getActivity());
 
         sharedPreferencesManager = new SharedPreferencesManager(getContext());
-        UserDto userDto = sharedPreferencesManager.getUser();
+        this.userDto = sharedPreferencesManager.getUser();
 
 
         long lastSync = sharedPreferencesManager.getLastSync();
@@ -120,7 +127,7 @@ public class UserDetailsFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-        UserDto userDto = sharedPreferencesManager.getUser();
+        this.userDto = sharedPreferencesManager.getUser();
         populateFields(userDto);
     }
 
@@ -135,21 +142,26 @@ public class UserDetailsFragment extends Fragment {
 
     public void pickImage() {
         ImagePicker.with(this)
-                .crop()//Crop image(Optional), Check Customization for more option
-                .compress(1024)            //Final image size will be less than 1 MB(Optional)
+                .crop()
+                .compress(1024)
                 .maxResultSize(1080, 1080)
                 .start(IMAGE_PICKER_REQUEST);
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == IMAGE_PICKER_REQUEST && resultCode == Activity.RESULT_OK) {
             Uri sourceUri = data.getData();
             if (sourceUri != null) {
-                uploadAvatar(sourceUri);
-            } else {
-                Toast.makeText(getContext(), "Error Occurred!", Toast.LENGTH_SHORT).show();
+                try {
+                    final InputStream imageStream = getContext().getContentResolver().openInputStream(sourceUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    userDto.setAvatar(encodeImage(selectedImage));
+                    uploadAvatar(userDto);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         } else if (resultCode == ImagePicker.RESULT_ERROR) {
             Toast.makeText(getContext(), ImagePicker.getError(data), Toast.LENGTH_SHORT).show();
@@ -158,9 +170,30 @@ public class UserDetailsFragment extends Fragment {
         }
     }
 
-    public void uploadAvatar(Uri uri) {
+    private String encodeImage(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] b = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(b, Base64.DEFAULT);
+    }
+
+    public void uploadAvatar(UserDto userDto) {
         pd.setMessage("Uploading Avatar ...");
         pd.show();
 
+        userViewModel.hitUpdateUser(getActivity(), userDto).observe(getViewLifecycleOwner(), responseDTO -> {
+            pd.dismiss();
+            switch (responseDTO.getStatus()) {
+                case "success":
+                    this.userDto = sharedPreferencesManager.getUser();
+                    Utils.loadAvatar(userDto, imageViewAvatar);
+                    Snackbar.make(getView(), responseDTO.getMessage(), Snackbar.LENGTH_LONG).show();
+                    break;
+                case "failed":
+                case "error":
+                    Snackbar.make(getView(), responseDTO.getMessage(), Snackbar.LENGTH_LONG).show();
+                    break;
+            }
+        });
     }
 }
